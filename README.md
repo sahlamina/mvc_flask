1f6705a33fac3cd1e99f63c55c7bc010e8dc0bb1
Saheed Lamina git config --global user.email you@example.comgit config --global user.name Saheed <agbolamina@hotmail.com>
2020-07-21 21:37:29 +0100

    Bootsrapped extra functionalities

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 #MVC - Model View Controllers
-* Not only used in python, also used in java c+
+* Not only used in python, also used in javascript, HTML CSS
 * In order for us to show our work, we use a web browser
 * Controller - needs to display information in view, which is the web browser. It is similar to the run file displaying info in the terminal
 ```` app.run()````
@@ -12,3 +12,11 @@ def welcome_user(username):
     return f"Welcome to python flask app, dear {username}"
 ````
 * 3 amigos, persona, Information Radiators with example, scrum artefacts, scrum events, scrum roles, sprint review and sprint retrospective, Starfish retrospective, Root cause Analysis-part of retrospective, agile vales and principles. There are many more things in agile and scrum for which resources have been provided and you have done it practically like user story, epic etc. Have a solid answer ready for what is Agile?
+Model
+* bootstrap - lets us style our page using CSS
+* we can utilise the built in functions
+- bootstrapping is essentially c&ping code to use in your programme
+- S showed us a website with a toolbar and explained that the toolbar was a parent class and the other pages inherited from it
+
+- Syntax for inheritance in html:
+```{% extends "base.html"%}```
--- app.py
+++ app.py
@@ -1,27 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 # importing flask
+# importing render_template()
 
 # in order for us to udr flask, we need to create an instance of our app
-
+#
 app = Flask(__name__)
-# syntax to create flask instance
-
-# these are called decorators and the below is the syntax for decorators to create a web toute
-# block of code for default page
-@app.route("/")
-
-# created a method to display on home/ default page
-
-def index():
-    return "<h1>Welcome to MVC with flask</h1>" #<h1> is html code for creating headers
-print(index())
-#  index method will be called at the endpoint
-#  index method will display on our home page
-
-#  syntax to run our app
-# is name is the main file, run the app
-if __name__ == "__main__":
-    app.run(debug=True)
+# # syntax to create flask instance
+#
+# # these are called decorators and the below is the syntax for decorators to create a web route
+# # block of code for default page
+# @app.route("/")
+#
+# # created a method to display on home/ default page
+#
+# def index():
+#     return "<h1>Welcome to MVC with flask</h1>" #<h1> is html code for creating headers
+# print(index())
+# #  index method will be called at the endpoint
+# #  index method will display on our home page
+#
+# #  syntax to run our app
+# # is name is the main file, run the app
+# if __name__ == "__main__":
+#     app.run(debug=True)
 #     debug=True is a built in functionality that means we are directed to the route every time we make changes without rerunning the app, saves a lot of time
 
 # exercise - create a function called welcome_user
@@ -29,13 +30,24 @@ if __name__ == "__main__":
 # return "welcome to python flask app dear {user}
 
 # this is an example of how we can use formatting to enter dynamic
-@app.route("/<username>")
-def welcome_user(username):
-    return f"Welcome to python flask app, dear {username}"
+@app.route("/")
+def welcome_user():
+    return render_template("index.html")
 
+@app.route("/login")
+def welcome_user():
+    return
+    # login functionality wit GET POST methods of HTTP
+    #  import request to use the methods check status code
+    #  add control flow to redirect the user according to the status code received
 
 if __name__ == "main":
     app.run(debug=True)
+# when you see an error/ exception - look at the end
+
+
+
+
 
 
 
